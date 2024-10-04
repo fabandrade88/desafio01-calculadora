bfd2ebdb14bf63a5c8d6982e74c6fa200c277d7d
fabricioandrade88 <fabricio.solutions13@gmail.com>
2024-10-05 01:37:28 +0200

    calc 01

---
+++ src/components/Button/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+padding: 20px;
+border: 1px solid #cdcdcd;
+background-color: #00aaf0;
+color: #ffffff;
+font-size:24px;
+font-weight: 600;
+margin: 2px;
+flex: 1;
+`;
